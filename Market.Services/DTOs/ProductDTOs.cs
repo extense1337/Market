54d87b039f4bd5bb593b36484ab133dbef93df29
Maxim <mxm1337@rambler.ru>
2023-05-08 19:50:34 +0300

    rewrite dtos to records

---
+++ Market.Services/DTOs/ProductDTOs.cs
@@ -0,0 +1,12 @@
+﻿using Market.DomainEntities.Entities;
+using Market.DomainEntities.Enums;
+
+namespace Market.Services.DTOs;
+
+public class ProductDTOs
+{
+    public record ProductAddDto(string DisplayName, decimal Price, PricingType PricingType, string Description,
+        ICollection<ProductPicture> Pictures, User Seller);
+
+    public record ProductEditDto(int Id, string DisplayName, decimal Price, PricingType PricingType, string Description);
+}
